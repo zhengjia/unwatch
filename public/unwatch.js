92d3dba7912febd56d7cc5be4b79b6777024519b
zhengjia <jiazheng@live.com>
2011-05-22 17:36:55 -0500

    add date filter

--- public/unwatch.js
+++ public/unwatch.js
@@ -7,17 +7,33 @@ $(document).ready(function(){
 		  type: "POST",
 			dataType: "text",
 		  url: "/unwatch/" + $(this).data('username') + '/' + $(this).data('repo'),
-			statusCode: {
-				200: function(msg){
-					$("#notice").html(msg);
-					$(that).parent().parent().hide();
-				},
-				503: function(msg){
-					$("#notice").html("Error occured");
-				}	
+			success: function(msg){
+				$("#notice").html(msg);
+				$(that).parent().parent().remove();
+			},
+			error:function(xhr, ajaxOptions, thrownError){
+			  $("#notice").html(xhr.responseText);
 			}
 		});
 		return false;
 	})
 	
+	$('#datePicker').datepicker();
+	$("#datePicker").bind("change", function(){
+		$('tr').show()
+		var date_str = $(this).val()
+		var date = Date.parse(date_str)
+		if( jQuery.trim( date_str ) != "" ){
+			$(".pushDate").each(function(){
+				if(Date.parse( $(this).text() ) < date){
+					$(this).parent().hide();
+				}
+			})
+		}
+		else{
+			$('tr').show()
+		}
+		
+	});
+
 })
