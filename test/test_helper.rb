d8f5d8514f1df2b5b94b8a0b9d804785402b5d6b
zhengjia <jiazheng@live.com>
2011-05-21 19:54:54 -0500

    remove a method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,15 +21,6 @@ def mock_app(base=Sinatra::Base, &block)
   @app = Sinatra.new(base, &block)
 end
 
-def oauth_stub(&block)
-  define_method "app" do
-    Unwatch.class_eval do
-      helpers &block
-    end
-    Unwatch
-  end
-end
-
 def fixture_watched
   {"repositories"=>[{"has_issues"=>true, "owner"=>"mojombo", "pushed_at"=>"2011/05/07 19:32:33 -0700", "watchers"=>952, "has_downloads"=>true, "forks"=>130, "description"=>"Grit gives you object oriented read/write access to Git repositories via Ruby.", "fork"=>false, "size"=>4498, "name"=>"grit", "has_wiki"=>true, "language"=>"Ruby", "url"=>"https://github.com/mojombo/grit", "private"=>false, "open_issues"=>43, "homepage"=>"http://grit.rubyforge.org/", "created_at"=>"2007/10/29 07:37:16 -0700"}, {"has_issues"=>true, "owner"=>"evanphx", "pushed_at"=>"2011/05/19 21:27:31 -0700", "watchers"=>1002, "has_downloads"=>true, "forks"=>160, "description"=>"Rubinius, the Ruby VM", "fork"=>false, "size"=>15384, "name"=>"rubinius", "has_wiki"=>true, "language"=>"Ruby", "url"=>"https://github.com/evanphx/rubinius", "private"=>false, "open_issues"=>37, "master_branch"=>"master", "homepage"=>"http://rubini.us", "created_at"=>"2008/01/12 08:46:52 -0800"} ]}
 end
